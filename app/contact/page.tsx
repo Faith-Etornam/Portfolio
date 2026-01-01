f662b0f528d52886648475fa2275f90b6a54b517
Faith Etornam <faithgbadegbe1@gmail.com>
2026-01-01 22:33:48 +0000

    Added SEO for the contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,26 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+
+  title: "Contact Me",
+
+  description:
+    "Get in touch with Faith Etornam. Available for backend engineering roles, API development projects, Frontend Development and technical collaborations.",
+
+  openGraph: {
+    title: "Get in Touch | Faith Etornam",
+    description:
+      "Let's build something scalable together. Connect with me for backend opportunities.",
+    url: "/contact",
+  },
+};
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+    </div>
+  );
+};
+
+export default Contact;
