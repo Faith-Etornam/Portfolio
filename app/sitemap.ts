05035c665aaded89591c44db6fa35dd03fced33f
Faith Etornam <faithgbadegbe1@gmail.com>
2026-01-01 22:59:56 +0000

    Added the projects page

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,34 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://localhost:3000";
-    return [
-        {
-            'url': baseUrl,
-            lastModified: new Date(),
-            changeFrequency: "monthly",
-            priority: 1
-        }
-    ]
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://localhost:3000";
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+
+    {
+        url: `${baseUrl}/services`,
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.8
+    },
+  ];
 }
