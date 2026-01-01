0a751a48ec3b8ac63eaee26c846c53c41f82dbce
Faith Etornam <faithgbadegbe1@gmail.com>
2026-01-01 23:02:41 +0000

    Finished SEO with the projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,12 +1,21 @@
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+    
+  title: "My Projects",
+  description:
+    "Explore the technical portfolio of Faith Etornam. Featuring scalable backend APIs, secure database architectures, and full-stack applications built with Python, Django, and React.js.",
 
+  openGraph: {
+    title: "Projects | Faith Etornam",
+    description:
+      "A showcase of backend engineering projects and dynamic web applications. View source code and live demos.",
+    url: "/projects",
+  },
+};
 
 const Projects = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default Projects
+export default Projects;
