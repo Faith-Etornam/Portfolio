04a8c48f9d4f26fa3178425db7e375010395678c
Faith Etornam <faithgbadegbe1@gmail.com>
2026-01-02 00:46:34 +0000

    Building the Navbar from scratch

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import Link from "next/link";
-import Image from "next/image"; 
-import { usePathname } from "next/navigation";
+import styles from './Navbar.module.css'
 
 const navLinks = [
   { href: "/", label: "Home" },
@@ -14,10 +12,13 @@ const navLinks = [
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
+   <header className={`${styles.header}`}>
+    <nav>
+
+    </nav>
+
+   </header>
+  );
 }
 
 export default Navbar
