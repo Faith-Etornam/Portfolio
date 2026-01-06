33a50bd5e4cec291a6ca39087ebc6975c79878e3
Faith Etornam <faithgbadegbe1@gmail.com>
2026-01-06 22:07:58 +0000

    Added the Layout component for handling the layout of each page

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react'
+import Navbar from '../Navbar';
+import Footer from '../Footer';
+
+interface Props {
+    children: ReactNode
+}
+
+const Layout = ({children}: Props) => {
+  return (
+    <div className="min-h-screen flex flex-col bg-background">
+      <Navbar />
+      <main className="flex-1 pt-20">{children}</main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout
