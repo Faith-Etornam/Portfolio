b00c629aa00c8566394e16bd00a3424da9686066
Faith Etornam <faithgbadegbe1@gmail.com>
2026-02-02 22:53:21 +0000

    Switching to tailwind css for faster development

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import Navbar from "../components/Navbar/Navbar";
-import Hero from "../components/Hero";
-
 export default function Home() {
   return (
     <>
-    <Navbar />
-    <Hero />
-    
+      
     </>
-  )
+  );
 }
