ea8da50d0212578fc2cb8917765d4777756e9da0
Faith Etornam <faithgbadegbe1@gmail.com>
2026-02-10 02:12:16 +0000

    Improving SEO for searches

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,6 +90,27 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+<script
+  type="application/ld+json"
+  dangerouslySetInnerHTML={{
+    __html: JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "Person",
+      name: "Faith Etornam",
+      url: "https://portfolio-ashen-psi.vercel.app",
+      image: "https://portfolio-ashen-psi.vercel.app/profile.jpg",
+      sameAs: [
+        "https://www.linkedin.com/in/faith-etornam",
+        "https://github.com/faith-etornam",
+      ],
+      jobTitle: "Backend Engineer",
+      worksFor: {
+        "@type": "Organization",
+        name: "Freelance"
+      }
+    }),
+  }}
+/>
         {children}
       </body>
     </html>
