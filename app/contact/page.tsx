fd0d49ff18f0241ca1c01e1b0c1364cc0608c9a1
Faith Etornam <faithgbadegbe1@gmail.com>
2026-03-04 20:54:22 +0000

    Added SEO to the contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,28 @@
 import ContactSection from "@/components/Contact/ContactSection";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact Me",
+  description:
+    "Get in touch with Faith Etornam to discuss scalable backend APIs, secure database architectures, and full-stack applications built with Python, Django, and React.js.",
+
+  openGraph: {
+    title: "Contact | Faith Etornam",
+    description:
+      "Have a project in mind or looking for a backend engineer? Reach out to Faith Etornam to discuss potential collaborations and technical solutions.",
+    url: "/contact",
+    images: [
+      {
+        url: "/opengraph-image",
+        width: 1200,
+        height: 630,
+        alt: "Contact Faith Etornam",
+      },
+    ],
+  },
+};
 
 const ContactPage = () => {
   return (
